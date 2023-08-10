924e34a8695f5a96ccc9683e43ee82344e4733d8
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-10 06:48:11 -0300

    CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import DismissAlert from "./components/DismissAlert"
-import Button from "./components/Button"
-import { useState } from "react"
+import ListGroup from "./components/ListGroup"
+
+const handleSelectItem = (item: string) => {
+  console.log(item)
+}
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false)
+  let cities = ["New York", "San Francisco", "Tokyo"]
 
   return (
     <div>
-      {alertVisible && (
-        <DismissAlert onClose={() => setAlertVisibility(false)}>
-          My Alert
-        </DismissAlert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>ButtonNN</Button>
+      <ListGroup
+        onSelectItem={handleSelectItem}
+        items={cities}
+        heading={"cities"}
+      ></ListGroup>
     </div>
   )
 }
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import styles from "./ListGroup.module.css"
 
 interface ListGroupProps {
   items: string[]
@@ -15,7 +16,7 @@ function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
-      <ul className="list-group">
+      <ul className={[styles.listGroup, styles.container].join(" ")}>
         {items.map((item, index) => (
           <li
             className={
