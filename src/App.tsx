8c86ac7ea99bd776810efefe5d5146bc8997817a
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-16 01:17:19 -0300

    adding an expense

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,11 @@ const App = () => {
   return (
     <div>
       <div className="mb-5">
-        <ExpenseForm />
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
       </div>
 
       <ExpenseFilter
