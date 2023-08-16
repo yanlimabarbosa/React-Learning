8617ecffe635634c937afa42d23ceb47b34c1e2c
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-16 04:56:06 -0300

    effect clean up

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { useEffect, useState } from "react"
-import ProductList from "./components/ProductList"
+import { useEffect } from "react"
+
+const connect = () => console.log("Connecting")
+const disconnect = () => console.log("Disconnecting")
 
 const App = () => {
-  const [category, setCategory] = useState("")
+  useEffect(() => {
+    connect()
 
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  )
+    return () => disconnect()
+  })
+
+  return <div></div>
 }
 export default App
