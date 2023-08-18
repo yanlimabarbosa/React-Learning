0cf898f3c6b9700740357f182ed3f7d20a983a4f
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-17 21:02:23 -0300

    deleting data

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -36,7 +36,24 @@ const App = () => {
     const originalUsers = [...users]
     setUsers(users.filter((u) => u.id !== user.id))
     axios
-      .delete(`https://jsonplaceholder.typicode.com/uxsers/${user.id}`)
+      .delete(`https://jsonplaceholder.typicode.com/users/${user.id}`)
+      .catch((err) => {
+        setError(err.message)
+        setUsers(originalUsers)
+      })
+  }
+
+  const AddUser = () => {
+    const originalUsers = [...users]
+    const newUser = { id: 0, name: "Mosh" }
+    setUsers([newUser, ...users])
+
+    axios
+      .post(
+        "https://jsonplaceholder.typicode.com/users",
+        newUser
+      )
+      .then((res) => setUsers([res.data, ...users]))
       .catch((err) => {
         setError(err.message)
         setUsers(originalUsers)
@@ -47,6 +64,9 @@ const App = () => {
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={AddUser}>
+        Add
+      </button>
       <ul className="list-group">
         {users.map((user) => (
           <li
