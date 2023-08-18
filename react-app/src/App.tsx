caf7df993d66b367633e68dabfff866cd6eddf7f
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-18 04:19:03 -0300

    custom data fetching hook created

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,29 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { useEffect, useState } from "react"
 import userService, { User } from "./services/user-service"
-import { CanceledError } from "axios"
+import useUsers from "./hooks/useUsers"
 
 const App = () => {
-  const [users, setUsers] = useState<User[]>([])
-  const [error, setError] = useState("")
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    const { request, cancel } = userService.getAll<User>()
-    request
-      .then((res) => {
-        setUsers(res.data)
-        setLoading(false)
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return
-        setError(err.message)
-        setLoading(false)
-      })
-
-    return () => cancel()
-  }, [])
+  const { users, error, isLoading, setUsers, setError } = useUsers()
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users]
