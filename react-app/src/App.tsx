a32089de158365c04f45efe24ecac4833c168855
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-18 04:11:45 -0300

    Generic HTTP Service created

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { useEffect, useState } from "react"
 import userService, { User } from "./services/user-service"
+import { CanceledError } from "axios"
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([])
@@ -9,7 +10,7 @@ const App = () => {
 
   useEffect(() => {
     setLoading(true)
-    const { request, cancel } = userService.getAllUsers()
+    const { request, cancel } = userService.getAll<User>()
     request
       .then((res) => {
         setUsers(res.data)
@@ -28,7 +29,7 @@ const App = () => {
     const originalUsers = [...users]
     setUsers(users.filter((u) => u.id !== user.id))
 
-    userService.deleteUser(user.id).catch((err) => {
+    userService.delete(user.id).catch((err) => {
       setError(err.message)
       setUsers(originalUsers)
     })
@@ -39,7 +40,7 @@ const App = () => {
     const newUser = { id: 0, name: "Mosh" }
     setUsers([newUser, ...users])
     userService
-      .createUser(newUser)
+      .create(newUser)
       .then((res) => setUsers([res.data, ...users]))
       .catch((err) => {
         setError(err.message)
@@ -51,7 +52,7 @@ const App = () => {
     const originalUsers = [...users]
     const updatedUser = { ...user, name: user.name + "!" }
     setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)))
-    userService.updateUser(updatedUser).catch((err) => {
+    userService.update(updatedUser).catch((err) => {
       setError(err.message)
       setUsers(originalUsers)
     })
