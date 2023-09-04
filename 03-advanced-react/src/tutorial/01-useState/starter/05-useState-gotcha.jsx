685e850332fadb117016a180f0006e1899a4cdf2
Yan Lima Barbosa <yanbr763@gmail.com>
2023-09-04 20:38:36 -0300

    useState gotcha

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,20 @@
-const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
-};
+import { useState } from "react"
 
-export default UseStateGotcha;
+const UseStateGotcha = () => {
+  const [value, setValue] = useState(0)
+  const handleClick = () => {
+    setTimeout(() => {
+      console.log("clicked the button")
+      setValue((currentState) => currentState + 1)
+    }, 3000)
+  }
+
+  return (
+    <>
+      <h2>{value}</h2>
+      <button onClick={handleClick}>Increase</button>
+    </>
+  )
+}
+
+export default UseStateGotcha
