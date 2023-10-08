0c84fb93a2c5e6dd9a1d6d6da688194aa7610735
Yan Lima Barbosa <yanbr763@gmail.com>
2023-10-08 13:04:05 -0300

    Project Structure Example

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,8 +1,10 @@
-import Navbar from "./tutorial/04-project-structure/starter/Navbar"
+import { Home, About } from "./tutorial/04-project-structure/starter/Pages"
+
 function App() {
   return (
     <div className="container">
-      <Navbar />
+      <Home />
+      <About />
     </div>
   )
 }
