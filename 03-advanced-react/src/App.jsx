11a2d9bbb325830472e1085231cac451725047cd
Yan Lima Barbosa <yanbr763@gmail.com>
2023-10-08 13:21:23 -0300

    Project Structure Export Group

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,10 +1,12 @@
-import { Home, About } from "./tutorial/04-project-structure/starter/Pages"
+import Example from "./tutorial/04-project-structure/starter/Example"
 
 function App() {
   return (
     <div className="container">
-      <Home />
-      <About />
+      <Example />
+      <div>
+        <h3>hello world</h3>
+      </div>
     </div>
   )
 }
