2787207a154f5dde24cbabd73fcef430b7b0acde
Yan Lima Barbosa <yanbr763@gmail.com>
2023-10-26 19:49:14 -0300

    useReducer start

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,13 @@
-import Navbar from "./tutorial/09-context-api/starter/Navbar.jsx"
+import Starter from "./tutorial/10-useReducer/starter/01-useReducer"
+import Final from "./tutorial/10-useReducer/final/01-useReducer"
 
 function App() {
-  return <Navbar />
+  return (
+    <div className="container">
+      <Starter />
+      {/* <Final /> */}
+    </div>
+  )
 }
 
 export default App
