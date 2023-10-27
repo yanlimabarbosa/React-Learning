3c1be3c2aebcf64b8d5e6055815c3e2684ff32e6
Yan Lima Barbosa <yanbr763@gmail.com>
2023-10-26 21:23:00 -0300

    Feat: Reducer: CLEAR_LIST, RESET_LIST, REMOVE_ITEM

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -5,29 +5,43 @@ const defaultState = {
   people: data,
 }
 
+const CLEAR_LIST = "CLEAR_LIST"
+const RESET_LIST = "RESET_LIST"
+const REMOVE_ITEM = "REMOVE_ITEM"
+
 const reducer = (state, action) => {
   if (action.type === "CLEAR_LIST") {
-    return {
-      ...state,
-      people: [],
-    }
+    return { ...state, people: [] }
   }
+  if (action.type === "RESET_LIST") {
+    return { ...state, people: data }
+  }
+  if (action.type === "REMOVE_ITEM") {
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id
+    )
+    return { ...state, people: newPeople }
+  }
+  // return state
+  throw new Error(`No match for "${action.type}" - action type`)
 }
 
 const ReducerBasics = () => {
   const [state, dispatch] = useReducer(reducer, defaultState)
 
   const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: { id } })
     // let newPeople = people.filter((person) => person.id !== id)
     // setPeople(newPeople)
   }
 
   const clearList = () => {
-    dispatch({ type: "CLEAR_LIST " })
+    dispatch({ type: CLEAR_LIST })
     // setPeople([])
   }
 
   const resetList = () => {
+    dispatch({ type: RESET_LIST })
     // setPeople(data)
   }
   return (
