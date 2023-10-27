cd56621c7ce3ee1cd52f19ae5f7dd3a7755f831c
Yan Lima Barbosa <yanbr763@gmail.com>
2023-10-26 21:26:43 -0300

    Refactor: useReducer Folders Import and Export

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,29 +1,10 @@
 import { useState, useReducer } from "react"
 import { data } from "../../../data"
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from "./actions"
+import reducer from "./reducer"
 
 const defaultState = {
   people: data,
-}
-
-const CLEAR_LIST = "CLEAR_LIST"
-const RESET_LIST = "RESET_LIST"
-const REMOVE_ITEM = "REMOVE_ITEM"
-
-const reducer = (state, action) => {
-  if (action.type === "CLEAR_LIST") {
-    return { ...state, people: [] }
-  }
-  if (action.type === "RESET_LIST") {
-    return { ...state, people: data }
-  }
-  if (action.type === "REMOVE_ITEM") {
-    let newPeople = state.people.filter(
-      (person) => person.id !== action.payload.id
-    )
-    return { ...state, people: newPeople }
-  }
-  // return state
-  throw new Error(`No match for "${action.type}" - action type`)
 }
 
 const ReducerBasics = () => {
