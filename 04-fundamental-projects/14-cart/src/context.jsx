ac542413047fb63492e4e5dcf3b273129f4f00d7
Yan Lima Barbosa <yanbr763@gmail.com>
2023-10-30 21:05:50 -0300

    Feat: Map Object, UseReducer: Clear Cart, Remove Item, Increase Amount, Decrease Amount, Calculate Totals and Fetch Structure

--- 04-fundamental-projects/14-cart/src/context.jsx
+++ 04-fundamental-projects/14-cart/src/context.jsx
@@ -1,11 +1,68 @@
 import { useContext, createContext, useReducer, useEffect } from "react"
+import reducer from "./reducer"
+import cartItems from "./data"
+
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./action"
+import { getTotals } from "./utils"
+const url = "https://www.course-api.com/react-useReducer-cart-project"
 
 const AppContext = createContext()
 
+const initialState = {
+  loading: false,
+  cart: new Map(),
+}
+
 export const AppProvider = ({ children }) => {
-  const greeting = "hello"
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { totalAmount, totalCost } = getTotals(state.cart)
+
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART })
+  }
+
+  const remove = (id) => {
+    dispatch({ type: REMOVE, payload: { id } })
+  }
+
+  const increase = (id) => {
+    dispatch({ type: INCREASE, payload: { id } })
+  }
+  const decrease = (id) => {
+    dispatch({ type: DECREASE, payload: { id } })
+  }
+
+  const fetchData = async () => {
+    dispatch({ type: LOADING })
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({ type: DISPLAY_ITEMS, payload: { cart } })
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        totalAmount,
+        totalCost,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
   )
 }
 
