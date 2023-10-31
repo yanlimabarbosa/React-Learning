86b73e44dc8e02e7449677597d4cd4bd13048123
Yan Lima Barbosa <yanbr763@gmail.com>
2023-10-30 23:01:19 -0300

    Performance: React.memo() and UseCallback Hook

--- 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,21 +1,29 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
+import { useState, useCallback } from "react"
+import { data } from "../../../../data"
+import List from "./List"
 const LowerState = () => {
-  const [people, setPeople] = useState(data);
-  const [count, setCount] = useState(0);
+  const [people, setPeople] = useState(data)
+  const [count, setCount] = useState(0)
+
+  const removePerson = useCallback(
+    (id) => {
+      const newPeople = people.filter((person) => person.id !== id)
+      setPeople(newPeople)
+    },
+    [people]
+  )
 
   return (
     <section>
       <button
-        className='btn'
+        className="btn"
         onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
+        style={{ marginBottom: "1rem" }}
       >
         count {count}
       </button>
-      <List people={people} />
+      <List people={people} removePerson={removePerson} />
     </section>
-  );
-};
-export default LowerState;
+  )
+}
+export default LowerState
