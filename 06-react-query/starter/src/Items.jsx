604ca2bab0312537b8af25dd661559c5f9724d31
Yan Lima Barbosa <yanbr763@gmail.com>
2023-11-07 07:25:13 -0300

    React Query: First Query and Render Data

--- 06-react-query/starter/src/Items.jsx
+++ 06-react-query/starter/src/Items.jsx
@@ -1,11 +1,33 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import { useQuery } from "@tanstack/react-query"
+import SingleItem from "./SingleItem"
+import customFetch from "./utils"
+
+const Items = () => {
+  const { isLoading, data } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customFetch.get("/"),
+    /*
+    Alternative Approach:
+    
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await customFetch.get("/")
+      return data
+    },
+    */
+  })
+
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>
+  }
+
   return (
-    <div className='items'>
-      {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
+    <div className="items">
+      {data.data.taskList.map((item) => {
+        //{data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />
       })}
     </div>
-  );
-};
-export default Items;
+  )
+}
+export default Items
---
+++ 06-react-query/starter/src/utils.js
@@ -0,0 +1,5 @@
+import axios from "axios"
+
+const customFetch = axios.create({ baseURL: "http://localhost:5000/api/tasks" })
+
+export default customFetch
