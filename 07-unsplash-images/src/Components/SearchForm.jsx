5a1a9d1f6db95dcb84ec1acaaf537e01aa064805
Yan Lima Barbosa <yanbr763@gmail.com>
2023-11-08 16:10:21 -0300

    Unsplash Images: Search Form & Uncontrolled Input

--- 07-unsplash-images/src/Components/SearchForm.jsx
+++ 07-unsplash-images/src/Components/SearchForm.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react"
+
 const SearchForm = () => {
   return (
-    <div>SearchForm</div>
+    <section>
+      <h1 className="title">Unsplash Images</h1>
+      <form
+        className="search-form"
+        onSubmit={(e) => {
+          e.preventDefault()
+          const searchValue = e.target.elements.search.value
+          if (!searchValue) return
+          console.log(searchValue)
+        }}
+      >
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="form-input search-input"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
   )
 }
 export default SearchForm
