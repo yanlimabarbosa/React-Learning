f1b5035e50d95d1ac69609a823794dee4d734dce
Yan Lima Barbosa <yanbr763@gmail.com>
2023-11-12 02:02:25 -0300

    Unsplash Images: Initiate Page on preferred Theme

--- 07-unsplash-images/src/Context/context.jsx
+++ 07-unsplash-images/src/Context/context.jsx
@@ -2,18 +2,27 @@ import { createContext, useContext, useState, useEffect } from "react"
 
 const AppContext = createContext()
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches
+  const storedDarkMode = localStorage.getItem("darkTheme") === "true"
+  return storedDarkMode || prefersDarkMode
+}
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false)
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
   const [searchTerm, setSearchTerm] = useState("cat")
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme
     setIsDarkTheme(newDarkTheme)
-    const body = document.querySelector("body")
-    console.log(isDarkTheme)
-    document.body.classList.toggle("dark-theme", newDarkTheme)
+    localStorage.setItem("darkTheme", newDarkTheme)
   }
 
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme)
+  }, [isDarkTheme])
   return (
     <AppContext.Provider
       value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
