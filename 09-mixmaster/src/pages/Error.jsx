d8c445b1c15febe5a09aa9f2a054d3133cba8e47
Yan Lima Barbosa <yanbr763@gmail.com>
2023-11-26 13:23:11 -0300

    MixMaster: Error Page

--- 09-mixmaster/src/pages/Error.jsx
+++ 09-mixmaster/src/pages/Error.jsx
@@ -1,6 +1,28 @@
+import Wrapper from "../assets/wrappers/ErrorPage"
+import { Link, useRouteError } from "react-router-dom"
+import img from "../assets/not-found.svg"
+
 const Error = () => {
+  const error = useRouteError()
+  console.log(error)
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="page not found" />
+          <h3>Ohh!</h3>
+          <p>We can't seem to find the page you are looking for</p>
+          <Link to="/">Back Home</Link>
+        </div>
+      </Wrapper>
+    )
+  }
   return (
-    <div>Error</div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
   )
 }
 export default Error
