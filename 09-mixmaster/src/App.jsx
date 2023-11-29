519a7f9c3b24fbfb1cc5438ad0de3414d9044c2e
Yan Lima Barbosa <yanbr763@gmail.com>
2023-11-29 05:52:06 -0300

    MixMaster: UseNavigation and Minor

--- 09-mixmaster/src/App.jsx
+++ 09-mixmaster/src/App.jsx
@@ -10,6 +10,7 @@ import {
 } from "./pages"
 
 import { loader as landingLoader } from "./pages/Landing"
+import { loader as singleCocktailLoader } from "./pages/Cocktail"
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,8 @@ const router = createBrowserRouter([
       },
       {
         path: "/cocktail/:id",
+        errorElement: <SinglePageError />,
+        loader: singleCocktailLoader,
         element: <Cocktail />,
       },
       {
--- 09-mixmaster/src/pages/HomeLayout.jsx
+++ 09-mixmaster/src/pages/HomeLayout.jsx
@@ -1,12 +1,20 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigation } from "react-router-dom"
 import Navbar from "../components/Navbar"
 
 const HomeLayout = () => {
+  const navigation = useNavigation()
+  const isPageLoading = navigation.state === "loading"
+  const value = "value"
   return (
     <>
       <Navbar />
       <section className="page">
-        <Outlet />
+        {isPageLoading ? (
+          <div className="loading" />
+        ) : (
+          <Outlet />
+          // <Outlet context={{ value }} />
+        )}
       </section>
     </>
   )
