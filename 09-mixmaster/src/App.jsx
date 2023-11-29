cd346be587e3af108d85bfd96db2f05ec04fa74c
Yan Lima Barbosa <yanbr763@gmail.com>
2023-11-29 15:43:25 -0300

    MixMaster: React Query Approach

--- 09-mixmaster/src/App.jsx
+++ 09-mixmaster/src/App.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import {
   About,
   HomeLayout,
@@ -13,6 +15,14 @@ import { loader as landingLoader } from "./pages/Landing"
 import { loader as singleCocktailLoader } from "./pages/Cocktail"
 import { action as newLetterAction } from "./pages/Newsletter"
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,12 +33,12 @@ const router = createBrowserRouter([
         index: true,
         element: <Landing />,
         errorElement: <SinglePageError />,
-        loader: landingLoader,
+        loader: landingLoader(queryClient),
       },
       {
         path: "/cocktail/:id",
         errorElement: <SinglePageError />,
-        loader: singleCocktailLoader,
+        loader: singleCocktailLoader(queryClient),
         element: <Cocktail />,
       },
       {
@@ -45,6 +55,11 @@ const router = createBrowserRouter([
 ])
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 export default App
--- 09-mixmaster/src/pages/Cocktail.jsx
+++ 09-mixmaster/src/pages/Cocktail.jsx
@@ -3,16 +3,29 @@ import axios from "axios"
 import Wrapper from "../assets/wrappers/CocktailPage"
 const singleCocktailUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
+import { useQuery } from "@tanstack/react-query"
 
-export const loader = async ({ params }) => {
-  const { id } = params
-  const { data } = await axios.get(`${singleCocktailUrl}${id}`)
-
-  return { id, data }
+const singleCocktailQuery = (id) => {
+  return {
+    queryKey: ["cocktail", id],
+    queryFn: async () => {
+      const { data } = await axios.get(`${singleCocktailUrl}${id}`)
+      return data
+    },
+  }
 }
 
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    const { id } = params
+    await queryClient.ensureQueryData(singleCocktailQuery(id))
+    return { id }
+  }
+
 const Cocktail = () => {
-  const { id, data } = useLoaderData()
+  const { id } = useLoaderData()
+  const { data } = useQuery(singleCocktailQuery(id))
   if (!data) return <Navigate to="/" />
   const singleDrink = data.drinks[0]
   const {
